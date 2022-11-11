248fa827cb0cbe41b0981ab76ced9e77ebe10e47
bfddeveloper <dicksonbrody@icloud.com>
2022-11-10 20:13:57 -0600

    Initial Commit

---
+++ Wolf scorer and bet tracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Wolf scorer and bet tracker
+//
+//  Created by Brody Dickson on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wolf scorer and bet tracker/Wolf_scorer_and_bet_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wolf_scorer_and_bet_trackerApp.swift
+//  Wolf scorer and bet tracker
+//
+//  Created by Brody Dickson on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Wolf_scorer_and_bet_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
