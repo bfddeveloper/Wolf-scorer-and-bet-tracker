0dcd8c3e676a7598504f89df3422816edf700b47
bfddeveloper <dicksonbrody@icloud.com>
2022-11-11 09:09:15 -0600

    added crude player entry and second scorecard view

---
+++ SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Wolf scorer and bet tracker
+//
+//  Created by Brody Dickson on 11/11/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
---
+++ playerScoreCard.swift
@@ -0,0 +1,21 @@
+//
+//  playerScoreCard.swift
+//  Wolf scorer and bet tracker
+//
+//  Created by Brody Dickson on 11/11/22.
+//
+
+import SwiftUI
+
+struct playerScoreCard: View {
+    let players : Players
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct playerScoreCard_Previews: PreviewProvider {
+    static var previews: some View {
+        playerScoreCard(players: Players())
+    }
+}
