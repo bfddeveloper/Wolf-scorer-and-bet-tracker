1a1756f3e396f840dbee52df1750767090e03bd4
bfddeveloper <dicksonbrody@icloud.com>
2022-11-17 11:19:50 -0600

    Player entry + player array passed through to scorecard view

---
+++ ContentView.swift
@@ -0,0 +1,152 @@
+//
+//  ContentView.swift
+//  Wolf scorer and bet tracker
+//
+//  Created by Brody Dickson on 11/10/22.
+//
+
+import SwiftUI
+
+struct Players {
+    var Player1 = ""
+    var Player2 = ""
+    var Player3 = ""
+    var Player4 = ""
+    var Player5 = ""
+    
+    var Player1H = ""
+    var Player2H = ""
+    var Player3H = ""
+    var Player4H = ""
+    var Player5H = ""
+    
+   
+
+}
+
+
+
+
+
+struct ContentView: View {
+    @State private var players = Players()
+    @State private var playerArray = [String]()
+    @State private var isShowingDetailView = false
+
+    var body: some View{
+        NavigationView {
+            
+            VStack{
+                HStack{
+                    Text("Players")
+                    Spacer()
+                    Text("Handicap numbers")
+                }
+                HStack{
+                    Spacer()
+                    Text("1")
+                    CustomTextFieldPlayer(placeholder: "Player 1", variable: $players.Player1)
+                    CustomTextFieldHandi(placeholder: "#1", variable: $players.Player1H)
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("2")
+                    CustomTextFieldPlayer(placeholder: "Player 2", variable: $players.Player2)
+                    CustomTextFieldHandi(placeholder: "#2", variable: $players.Player2H)
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("3")
+                    CustomTextFieldPlayer(placeholder: "Player 3", variable: $players.Player3)
+                    CustomTextFieldHandi(placeholder: "#3", variable: $players.Player3H)
+                    Spacer()
+                    
+                }
+                HStack{
+                    Spacer()
+                    Text("4")
+                    CustomTextFieldPlayer(placeholder: "Player 4", variable: $players.Player4)
+                    CustomTextFieldHandi(placeholder: "#4", variable: $players.Player4H)
+                    Spacer()
+                    
+                }
+                HStack{
+                    Spacer()
+                    Text("5")
+                    CustomTextFieldPlayer(placeholder: "Player 5", variable: $players.Player5)
+                    CustomTextFieldHandi(placeholder: "#%", variable: $players.Player5H)
+                    Spacer()
+                    
+                }
+                VStack{
+                    NavigationLink(destination: playerScoreCard(players: players, playerArray: playerArray),  isActive: $isShowingDetailView) { EmptyView()}
+                    Button("Add Players") {
+                        isShowingDetailView = true
+                        playerArray.append(players.Player1)
+                        playerArray.append(players.Player2)
+                        playerArray.append(players.Player3)
+                        playerArray.append(players.Player4)
+                        playerArray.append(players.Player5)
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//struct playerHandicapEntry: View {
+//    @State private var players = Players()
+//    var body: some View{
+//        VStack{
+//            HStack{
+//                Text("Players")
+//                Text("Handicap numbers")
+//            }
+//            HStack{
+//                Text("1")
+//
+//                CustomTextFieldPlayer(placeholder: "Player 1", variable: $players.Player1)
+//
+//                CustomTextFieldHandi(placeholder: "#1", variable: $players.Player1H)
+//
+//            }
+//            HStack{
+//                Text("2")
+//                CustomTextFieldPlayer(placeholder: "Player 2", variable: $players.Player2)
+//
+//                CustomTextFieldHandi(placeholder: "#2", variable: $players.Player2H)
+//
+//            }
+//        }
+//    }
+//}
+
+struct CustomTextFieldPlayer: View {
+    let placeholder : String
+    let variable : Binding<String>
+    var body: some View {
+        TextField(placeholder, text: variable)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+    }
+}
+
+struct CustomTextFieldHandi: View {
+    let placeholder : String
+    let variable : Binding<String>
+    var body: some View {
+        TextField(placeholder, text: variable)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+            .frame(width: 25, height: 25)
+    }
+}
--- playerScoreCard.swift
+++ playerScoreCard.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
-
 struct playerScoreCard: View {
     let players : Players
+    let playerArray : [String]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack{
+                CustomTextWithSub(player: players.Player1, handi: players.Player1H)
+            }
+            Text(players.Player1)
+            Text(players.Player2)
+            Text(players.Player3)
+            Text(players.Player4)
+            Text(players.Player5)
+            Text("\(playerArray[0])")
+            
+            
+            
+            
+            //this doesnt work
+            
+            
+//                    .onAppear { // Prefer, Life cycle method
+//                        self.playerArray.append(players.Player1)
+//                           }
+        }
+    }
+    
+}
+struct CustomTextWithSub: View {
+    let player : String
+    let handi : String
+    var body: some View {
+        HStack{
+            Text(player)
+                .bold()
+            Text(handi)
+                .offset(x: 10, y: 12)
+                .font(.system(size: 12))
+                .fontWeight(.heavy)
+                
+            
+                
+            
+        }
+        
     }
 }
 
 struct playerScoreCard_Previews: PreviewProvider {
     static var previews: some View {
-        playerScoreCard(players: Players())
+        playerScoreCard(players: Players(), playerArray: ["Harry"])
     }
 }
