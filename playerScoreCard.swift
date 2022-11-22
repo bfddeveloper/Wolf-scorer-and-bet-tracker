680376b3e3dc1c0611d9afff36cd298e42db33e5
bfddeveloper <dicksonbrody@icloud.com>
2022-11-21 20:12:10 -0600

    added score board view, with editable text boxes

--- playerScoreCard.swift
+++ playerScoreCard.swift
@@ -10,33 +10,57 @@ struct playerScoreCard: View {
     let players : Players
     let playerArray : [String]
     let handicapArray :  [String]
+    @State private var frontholes = ["1","2","3","4","5","6","7","8","9"]
+    @State private var plyr1scorefirst = ["", "", "", "", "", "", "", "", ""]
+    @State private var plyr2scorefirst = ["", "", "", "", "", "", "", "", ""]
+    @State private var plyr3scorefirst = ["", "", "", "", "", "", "", "", ""]
+    @State private var plyr4scorefirst = ["", "", "", "", "", "", "", "", ""]
+    @State private var plyr5scorefirst = ["", "", "", "", "", "", "", "", ""]
+    @State private var plyr1totalfront = 0
     var body: some View {
         VStack{
             VStack{
-                CustomTextWithSub(player: players.Player1, handi: players.Player1H)
-            }
-//            Text(players.Player1)
-//            Text(players.Player2)
-//            Text(players.Player3)
-//            Text(players.Player4)
-//            Text(players.Player5)
-//            Text("\(playerArray[0])")
-            VStack{
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(0..<5) { index in
-                        CustomTextWithSub(player: playerArray[index], handi: handicapArray[index])
+                HStack{
+                    CustomTextWithSub(player: players.Player1, handi: players.Player1H)
+                    HStack{
+                        ForEach(0..<9){ index in
+                            TextField(frontholes[index], text: $plyr1scorefirst[index] )
+                                .frame(width: 20, height: 20)
+                        }
+                    }
+                    Text("\(plyr1totalfront)")
+                        .font(.system(size: 25))
+                        .bold()
+                }
+                
+
+                HStack{
+                    CustomTextWithSub(player: players.Player2, handi: players.Player2H)
+                    HStack{
+                        ForEach(0..<9){ index in
+                            TextField(frontholes[index], text: $plyr2scorefirst[index] )
+                                .frame(width: 25, height: 25)
+                        }
                     }
                 }
+                HStack{
+                    CustomTextWithSub(player: players.Player3, handi: players.Player3H)
+                    HStack{
+                        ForEach(0..<9){ index in
+                            TextField(frontholes[index], text: $plyr3scorefirst[index] )
+                                .frame(width: 25, height: 25)
+                        }
+                    }
+                }
+                
             }
-            
-            
-            
-            //this doesnt work
-            
-            
-//                    .onAppear { // Prefer, Life cycle method
-//                        self.playerArray.append(players.Player1)
-//                           }
+            VStack{
+                HStack{
+                        ForEach(0..<9){ index in
+                            Text(frontholes[index])
+                        }
+                }
+            }
         }
     }
     
@@ -52,12 +76,8 @@ struct CustomTextWithSub: View {
                 .offset(x: -5, y: 10)
                 .font(.system(size: 12))
                 .fontWeight(.heavy)
-                
-            
-                
-            
         }
-        
+        .frame(width: 100, height: 30, alignment: .leading)
     }
 }
 
@@ -71,3 +91,25 @@ let columns = [
     
         GridItem(.adaptive(minimum: 250)),
     ]
+                          
+let rows = [
+    
+    GridItem(.fixed(25))
+    
+]
+
+struct player1scoresheet {
+    var hole1 = ""
+    var hole2 = ""
+    var hole3 = ""
+    var hole4 = ""
+    var hole5 = ""
+    var hole6 = ""
+    var hole7 = ""
+    var hole8 = ""
+    var hole9 = ""
+
+
+}
+
+
